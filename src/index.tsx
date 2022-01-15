fc6308ccc52ba6c8e3184ce74f8c700ac2743b4d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-15 14:00:13 -0300

    create: app index

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import App from '.app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Pedro Sites</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
