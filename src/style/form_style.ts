40cf868f35945640179c8e34d7ff37eca44b26e4
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 16:53:44 -0300

    Create: Style - Wrapper (./style/form_style.ts)

---
+++ src/style/form_style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.main`
+  width: 100%;
+  height: 100vh;
+
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+
