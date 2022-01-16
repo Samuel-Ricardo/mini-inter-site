d0755a43c7569d409c65331ad53ebbc96a1be5ce
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 17:08:20 -0300

    Create: Style - ButtonContainer (./style/form_style.ts)

--- src/style/form_style.ts
+++ src/style/form_style.ts
@@ -29,7 +29,29 @@ export const Background = styled.div<{ image: any }>`
 export const InputContainer = styled.div`
   flex: 1;
   width: 90%;
-  margin-top: 67px;
+  margin-top: 67pt;
+`;
+
+export const ButtonContainer = styled.div`
+  width: 90%;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  margin-top: 20pt;
+
+  P{
+    font-size: 0.75rem;
+    font-weight: 400;
+    color: ${({ theme }) => theme.COLORS.SECONDARY};
+
+    a {
+      font-size: 1rem;
+      font-weight: 700;
+    }
+  }
+
 `;
 
 const form_style = {
