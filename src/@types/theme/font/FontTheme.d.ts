7437ec8c32b01852020ba88839a13706cebf8ef8
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 17:52:10 -0300

    Create: Type - FontTheme (./@types/theme/font)

---
+++ src/@types/theme/font/FontTheme.d.ts
@@ -0,0 +1,10 @@
+declare module '@Types/theme/font' {
+  export interface TextTheme {
+    FONT_FAMILY: string,
+    H1:TextTheme,
+    H2:TextTheme,
+    P:TextTheme,
+    LINK:TextTheme,
+    TEXT:TextTheme
+  }
+}
