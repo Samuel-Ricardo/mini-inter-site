caf730cb37032fd81846cf2ea53ce8f5803673a6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 17:46:21 -0300

    clean

--- src/style/themes/lite/index.ts
+++ src/style/themes/lite/index.ts
@@ -1,5 +1,6 @@
+import { DefaultTheme } from "styled-components";
 import { colors } from "./colors";
 
-export const lite_theme = {
+export const lite_theme:DefaultTheme = {
   COLORS:{...colors}
 }
