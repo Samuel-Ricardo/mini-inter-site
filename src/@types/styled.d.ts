fb5d54ef4541dd96289f46a8d601c5630cb55794
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 11:24:42 -0300

    Create Type - DefaultTheme

--- src/@types/styled.d.ts
+++ src/@types/styled.d.ts
@@ -1,5 +1,15 @@
 import 'styled-components';
 
 declare module 'styled-components' {
-  
+  export interface DefaultTheme {
+    colors: {
+      primary: string,
+      secondary: string,
+      tertiary: string,
+      background: string,
+      backgroundLight: string,
+      red: string,
+      green: string
+    }
+  }
 }
