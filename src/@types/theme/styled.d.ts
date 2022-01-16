ad7e27e817f4826ff73a958e2095def4cdad9baa
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 17:55:52 -0300

    Update: Type - styled

--- src/@types/theme/styled.d.ts
+++ src/@types/theme/styled.d.ts
@@ -1,3 +1,4 @@
+import { FontTheme } from '@Types/theme/font';
 import 'styled-components';
 
 declare module 'styled-components' {
@@ -11,6 +12,8 @@ declare module 'styled-components' {
       H2: string,
       RED: string,
       GREEN: string
-    }
+    },
+
+    FONT:FontTheme
   }
 }
