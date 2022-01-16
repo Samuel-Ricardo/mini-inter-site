0cc60dd6f133a419b90a0761e542e47eb29f1d76
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 17:46:27 -0300

    clean

---
+++ src/style/global/global_fonts.ts
@@ -0,0 +1,16 @@
+import { TextTheme } from "@Types/theme/text";
+
+const H1: TextTheme = { FONT_SIZE: `16pt` };
+const H2: TextTheme = { FONT_SIZE: `16pt` };
+const P: TextTheme = { FONT_SIZE: `16pt` };
+const LINK: TextTheme = { FONT_SIZE: `16pt` };
+const TEXT: TextTheme = { FONT_SIZE: `16pt` };
+
+export const global_fonts = {
+  FONT_FAMILY: `'Roboto', sans-serif`,
+  H1,
+  H2,
+  P,
+  LINK,
+  TEXT
+};
