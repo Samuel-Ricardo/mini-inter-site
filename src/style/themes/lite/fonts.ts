4675cc2051eac58776790a128c56521401b72301
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 17:58:43 -0300

    update

--- src/style/themes/lite/fonts.ts
+++ src/style/themes/lite/fonts.ts
@@ -1,4 +1,4 @@
 import { FontTheme } from "@Types/theme/font";
 import { global_fonts } from "../../global/global_fonts";
 
-export const fonts: FontTheme = { ...global_fonts };
+export const lite_fonts: FontTheme = { ...global_fonts };
