7b57cd26b634c70b11f8501e554a21166bc175d5
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 11:17:55 -0300

    Refact: colors.ts (lite)

--- src/style/theme/lite/colors.ts
+++ src/style/theme/lite/colors.ts
@@ -1,9 +1,11 @@
+import { global_colors } from "../../global_colors"
+
 export const colors = {
+  ...global_colors,
+
     PRIMARY: "#FB6910",
     SECONDARY: "#343233",
     TERTIARY: "#E8E8E8",
     BACKGROUND: "#FFFFFF",
     BACKGROUND2: "#F6F6F7",
-    RED: "#E72424",
-    GREEN: "#15b138"
 }
