7dee3d1b1f05e725c1b46fdd7336ee2bed508731
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-16 11:14:12 -0300

    Update: File - colors.ts

---
+++ src/style/theme/lite/colors.ts
@@ -0,0 +1,9 @@
+export const colors = {
+    PRIMARY: "#FB6910",
+    SECONDARY: "#343233",
+    TERTIARY: "#E8E8E8",
+    BACKGROUND: "#FFFFFF",
+    BACKGROUND2: "#F6F6F7",
+    RED: "#E72424",
+    GREEN: "#15b138"
+}
