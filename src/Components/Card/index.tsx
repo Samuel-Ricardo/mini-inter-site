c1e89132b5576a8ad99c3f240b62c0aca945d41e
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-18 17:08:56 -0300

    Create: Component - Card

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,3 +1,4 @@
+import { CardContainer } from "./style";
 
 interface CardProps {
   width?: string,
@@ -5,3 +6,18 @@ interface CardProps {
   height?: string,
   shadow?: boolean
 }
+
+const Card = ({
+  children,
+  width = '100%',
+  height = 'auto',
+  shadow = true,
+}: CardProps) => {
+  return (
+    <CardContainer width={width} heigth={height} shadow={shadow}>
+      {children}
+    </CardContainer>
+  )
+}
+
+export default Card;
