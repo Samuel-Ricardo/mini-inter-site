18305d46cb98f5a4cebcd1181c8ab6b9c699b394
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-18 08:56:04 -0300

    Create: Screen - SingIn

--- src/Pages/SingnIn/index.tsx
+++ src/Pages/SingnIn/index.tsx
@@ -1,10 +1,28 @@
 import { useNavigate } from "react-router-dom";
+import { singin_style } from "./style";
+
+import background_image from '../../assets/images/background-login.jpg';
+import Inter_logo from '../../assets/images/Inter-orange.png';
 
 const SingIn = () => {
   const navigate = useNavigate();
 
   const handleToSingIn = () => { navigate('/dashboard') };
 
+  const {
+    Wrapper,
+    Background,
+    ButtonContainer,
+    InputContainer
+  } = singin_style;
+
+  return (
+    <Wrapper>
+      <Background image={background_image}>
+        Mike assausque
+      </Background>
+    </Wrapper>
+  )
 }
 
 export default SingIn;
