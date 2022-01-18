7593a3cf4c1354d0bc967a2ff75f70d1c7d23590
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-18 17:03:01 -0300

    Create: Style - CardContainer

---
+++ src/Components/Card/style.ts
@@ -0,0 +1,27 @@
+import styled, { css } from "styled-components";
+
+export const CardContainer = styled.div<{
+  width: string;
+  heigth: string;
+  shadow: boolean;
+}>`
+
+  width: ${({ width }) => width};
+  height: ${(props) => props.heigth};
+
+  background: ${({ theme }) => theme.COLORS.BACKGROUND};
+
+  ${props => props.shadow && css`
+      box-shadow: 5pt 4pt 6pt rgba(0, 0, 0, 0.25)`
+  }
+
+  border-radius: 20pt;
+
+  padding: 20pt;
+
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  z-index: 5000;
+`
