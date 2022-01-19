4ef8e743de9a2987379da66675539b83425f792a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-19 10:09:28 -0300

    Refact

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,20 @@ import {
   Routes,
   Route
 } from 'react-router-dom';
+import { routes } from '../config/routes';
 import SingIn from '../Pages/SingnIn';
 
-export const Router = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<SingIn/>} />
-      <Route path='/login' element={<SingIn/>} />
-    </Routes>
-  </BrowserRouter>
-)
+const {LOGIN,  ROOT, SINGIN} = routes;
+
+export const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROOT} element={<SingIn />} />
+        <Route path={LOGIN} element={<SingIn />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 //
 //<Route path='/' element={<SingIn />} />
