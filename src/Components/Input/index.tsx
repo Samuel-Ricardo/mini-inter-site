a091fdb7239b29b9b2af5838cf8e9cbbda2f7f2a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-19 09:44:45 -0300

    Create: Component - Input

--- src/Components/Input/index.tsx
+++ src/Components/Input/index.tsx
@@ -1,5 +1,12 @@
-const Input = () => {
+import { InputHTMLAttributes } from "react";
+import { InputContainer } from "./style";
 
+const Input = (props: InputHTMLAttributes<HTMLInputElement>) => {
+  return (
+    <InputContainer>
+      <input {...props}/>
+    </InputContainer>
+  )
 }
 
 export default Input;
