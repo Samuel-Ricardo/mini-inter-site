9ba1e26910a5f16645c569619d755d52f9a4ab88
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-19 09:26:10 -0300

    Update: File - app_images.ts

---
+++ src/config/app_images.ts
@@ -0,0 +1,7 @@
+import background_image from '../../assets/images/background-login.jpg';
+import Inter_logo from '../../assets/images/Inter-orange.png';
+
+export const app_images = {
+  Inter_logo,
+  background_image
+}
