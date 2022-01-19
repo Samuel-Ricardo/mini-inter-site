e73f8d567a60bd398219acd04c12bd946c9482a5
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-19 09:12:15 -0300

    Create: Styled Component - HeaderContainer

---
+++ src/Components/Header/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.div`
+  width: 100%;
+  height: 90pt;
+
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND};
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
