8d9fc233a18da324925d2044dbc2ef148d77e32d
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-19 09:57:38 -0300

    Create: Component - Header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,5 +1,29 @@
-const Header = () => {
+import { useNavigate } from "react-router-dom";
+import { app_images } from "../../config/app_images";
+import { routes } from "../../config/routes";
+import UserCircle from "../UserCircle";
+import { HeaderContainer, HeaderWrapper, UserInfo } from "./style";
 
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleLogoff = () => { navigate(routes.SINGIN) }
+
+  return (
+    <HeaderContainer>
+      <HeaderWrapper>
+        <img src={app_images.Inter_logo} width={172} height={61} alt="inter logo" />
+        <UserInfo>
+        <UserCircle initials="SR" />
+        <div>
+          <p>Olá, <span className="primary-color font-bold">Pablo</span></p>
+          <strong>22001123-1</strong><br />
+          <a href="#" onClick={handleLogoff}>Sair</a>
+        </div>
+        </UserInfo>
+      </HeaderWrapper>
+    </HeaderContainer>
+  )
 }
 
 export default Header;
