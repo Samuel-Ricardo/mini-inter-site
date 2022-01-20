b48087d551305dc3ad10c0c33f08206adead0eeb
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-20 09:05:05 -0300

    Bugfix: Imports

--- src/config/app_images.ts
+++ src/config/app_images.ts
@@ -1,5 +1,5 @@
-import background_image from '../../assets/images/background-login.jpg';
-import Inter_logo from '../../assets/images/Inter-orange.png';
+import background_image from '../assets/images/background-login.jpg';
+import Inter_logo from '../assets/images/Inter-orange.png';
 
 export const app_images = {
   Inter_logo,
