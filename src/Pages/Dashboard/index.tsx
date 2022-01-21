5ddf6641dd8045e4fc49eb3c7286d730e3594100
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-21 15:11:21 -0300

    Update: Component - Dashboard

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -4,6 +4,7 @@ import Header from "../../Components/Header";
 import Input from "../../Components/Input";
 import { app_classes } from "../../config/style_classes";
 import { formatMoney } from "../../utils";
+import Statement from "./Statement";
 import { BodyContainer, DashboardBackground, InlineContainer, InlineTitle } from "./style";
 
 const Dashboard = () => {
@@ -47,6 +48,14 @@ const Dashboard = () => {
               <Input style={{ flex: 1 }} />
               <Button>Pagar PIX</Button>
             </InlineContainer>
+          </Card>
+        </div>
+        <div>
+          <Card shadow={false} width="90%">
+            <InlineTitle>
+              <h2 className={H2}>Extrato da Conta</h2>
+            </InlineTitle>
+            <Statement/>
           </Card>
         </div>
       </BodyContainer>
