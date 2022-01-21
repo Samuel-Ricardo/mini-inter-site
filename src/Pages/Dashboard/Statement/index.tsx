61580c9cba9f1fea8b19429d4da251b93068b050
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-21 13:04:28 -0300

    Create: Component - StatementItem

--- src/Pages/Dashboard/Statement/index.tsx
+++ src/Pages/Dashboard/Statement/index.tsx
@@ -1,8 +1,21 @@
-import { Statement } from "@Types/components/props";
+import { Statement } from "@Types/models";
+import { app_classes } from "../../../config/style_classes";
+import { StatementItemContainer, StatementItemImage, StatementItemInfo } from "./style";
 
 const StatementItem = ({type, updatedAt, user, value}: Statement) => {
   return (
+    <StatementItemContainer>
+      <StatementItemImage type={type}>
 
+      </StatementItemImage>
+      <StatementItemInfo>
+        <p className={app_classes.PRIMARY_COLOR}>
+          {value.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}
+        </p>
+        <p>{type === 'pay' ? `Pago a` : `Recebido de`} <strong>{user.first_name} {user.last_name}</strong> </p>
+        <p></p>
+      </StatementItemInfo>
+    </StatementItemContainer>
   )
 }
 export default StatementItem;
