288693a1b48a87ab532b0a1fb716e9da81a401db
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-21 14:41:12 -0300

    Create: Component - Statement

--- src/Pages/Dashboard/Statement/index.tsx
+++ src/Pages/Dashboard/Statement/index.tsx
@@ -1,5 +1,56 @@
+import { statement } from "@Types/models";
+import StatementItem from "./Item";
+import { StatementContainer } from "./style";
+
 const Statement = ()  => {
 
+  const statements:statement[] = [
+
+    {
+      user:{
+          first_name: 'Pablo',
+          last_name: 'Henrique'
+      },
+      value: 250.00,
+      type: 'pay',
+      updatedAt: new Date()
+  },
+  {
+      user:{
+          first_name: 'José',
+          last_name: 'Santos'
+      },
+      value: 270.00,
+      type: 'received',
+      updatedAt: new Date()
+    },
+
+    {
+      user:{
+          first_name: 'Pablo',
+          last_name: 'Henrique'
+      },
+      value: 250.00,
+      type: 'pay',
+      updatedAt: new Date()
+  },
+  {
+      user:{
+          first_name: 'José',
+          last_name: 'Santos'
+      },
+      value: 270.00,
+      type: 'received',
+      updatedAt: new Date()
+  }
+
+  ]
+
+  return (
+    <StatementContainer>
+      {statements.map(statement => <StatementItem {...statement}/>)}
+    </StatementContainer>
+  )
  }
 
- export Statement;
+ export default Statement;
