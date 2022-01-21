e2f4567b668e81061cd468719155f4d3b1ac2612
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-21 13:35:45 -0300

    refact

--- src/Pages/Dashboard/Statement/Item/style.ts
+++ src/Pages/Dashboard/Statement/Item/style.ts
@@ -1,13 +1,5 @@
 import styled from "styled-components";
 
-export const StatementContainer = styled.div`
-  width:100%;
-  margin-top: 20%;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-`;
-
 export const StatementItemContainer = styled.div`
     display: flex;
     width: 100%;
