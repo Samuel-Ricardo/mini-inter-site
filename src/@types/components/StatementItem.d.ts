3bf225d0fbc5df22561a4e14745bb114a1892ad3
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-21 12:41:45 -0300

    Create: Type - StatementItemProps

--- src/@types/components/StatementItem.d.ts
+++ src/@types/components/StatementItem.d.ts
@@ -1,3 +1,11 @@
 declare module '@Types/components/props' {
-  
+  export interface StatementItemProps {
+    user: {
+      first_name: string,
+      last_name: string
+    },
+    value: number,
+    type: 'pay' | 'received',
+    updatedAt: Date
+  }
 }
