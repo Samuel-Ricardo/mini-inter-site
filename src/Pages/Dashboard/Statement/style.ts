ece463aa4b53af098121c092cdc0dc36e90a06f7
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-21 12:33:55 -0300

    Create: Styled Component - StatementItemImage (.../Statement/style.ts)

--- src/Pages/Dashboard/Statement/style.ts
+++ src/Pages/Dashboard/Statement/style.ts
@@ -16,3 +16,24 @@ export const StatementItemContainer = styled.div`
 export const StatementItemInfo = styled.div`
     p{ margin-bottom: 2px; }
 `
+
+export const StatementItemImage = styled.div<{
+  type: 'pay' | 'received'
+}>`
+
+  width: 60pt;
+  height: 60pt;
+
+  margin-right: 20pt;
+  margin-top: 20pt;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 10pt;
+
+  color: ${({ theme }) => theme.COLORS.BACKGROUND};
+
+  background-color: ${({ type, theme }) => type === 'pay' ? theme.COLORS.RED : theme.COLORS.GREEN};
+`;
