99a92340e554d73f6b488be26ddba41bba19bddd
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-21 11:32:47 -0300

    Create: Page - SignUp

--- src/Pages/SingnUp/index.tsx
+++ src/Pages/SingnUp/index.tsx
@@ -1,5 +1,14 @@
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+
+import Button from "../../Components/Button";
+import Card from "../../Components/Card";
+import Input from "../../Components/Input";
+
+import { app_images } from "../../config/app_images";
 import { routes } from "../../config/routes";
+
+import { signup_style } from "./style";
 
 const SignUp = () => {
 
@@ -9,8 +18,36 @@ const SignUp = () => {
     navigate(routes.DASHBOARD)
   }
 
+  const {
+    Wrapper,
+    Background,
+    ButtonContainer,
+    InputContainer
+  } = signup_style;
+
   return (
-    <></>
+    <Wrapper>
+      <Background image={app_images.background_image}/>
+      <Card width="403pt">
+        <img src={app_images.Inter_logo} width={172} height={61} alt="logo inter" />
+
+        <InputContainer>
+
+          <Input placeholder="NOME"/>
+          <Input placeholder="SOBRENOME"/>
+          <Input placeholder="EMAIL"/>
+          <Input placeholder="SENHA" type="password"/>
+          <Input placeholder="CONFIRMAR SENHA" type="password"/>
+
+        </InputContainer>
+
+        <ButtonContainer>
+          <Button type="button" onClick={handleToDashboard}>CADASTRE-SE</Button>
+          <p>Já tem uma conta? <Link to={routes.SIGNIN}>Entre já</Link></p>
+        </ButtonContainer>
+
+      </Card>
+    </Wrapper>
   )
 }
 
