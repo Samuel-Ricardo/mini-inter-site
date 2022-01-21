4a7f551aff2bfd0c8db2ad9c2605a2bc6437c5d2
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-21 12:16:54 -0300

    Create: Styled Component - StatementContainer (.../Statement/style.ts)

--- src/Pages/Dashboard/Statement/style.ts
+++ src/Pages/Dashboard/Statement/style.ts
@@ -1,1 +1,9 @@
+import styled from "styled-components";
 
+export const StatementContainer = styled.div`
+  width:100%;
+  margin-top: 20%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
