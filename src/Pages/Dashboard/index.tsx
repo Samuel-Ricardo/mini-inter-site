28ed7225338f21f12208e698cf75ea83eb42c140
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-21 15:00:52 -0300

    Create: Component - Dashboard

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,4 +1,27 @@
+import Button from "../../Components/Button";
+import Card from "../../Components/Card";
+import Header from "../../Components/Header";
+import Input from "../../Components/Input";
+import { app_classes } from "../../config/style_classes";
+import { formatMoney } from "../../utils";
+import { BodyContainer, DashboardBackground, InlineContainer, InlineTitle } from "./style";
+
 const Dashboard = () => {
+
+  const wallet = 1000.00;
+  const {H2, WALLET} = app_classes;
+
+  return (
+    <DashboardBackground>
+      <Header/>
+
+      <BodyContainer>
+        <div>
+
+        </div>
+      </BodyContainer>
+    </DashboardBackground>
+  )
 
 }
 
