ea794b4fbe4a983764852c2aa7895364a2a2af41
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 12:38:41 -0300

    Update: File - user.ts

--- src/service/resources/user.ts
+++ src/service/resources/user.ts
@@ -1,8 +1,8 @@
 import { SignInDTO, SignUpDTO } from "@Types/DTO";
-import { API_ROUTES } from "../../config/api";
+import { USER_ROUTES } from "../../config/api";
 import { API } from "../API";
 
 
-export const singIn = async (data: SignInDTO) => API.post(API_ROUTES.SIGNIN, data)
-export const singUp = async (data: SignUpDTO) => API.post(API_ROUTES.SIGNUP, data)
-export const me = async () => API.post(API_ROUTES.ME);
+export const singIn = async (data: SignInDTO) => API.post(USER_ROUTES.SIGNIN, data)
+export const singUp = async (data: SignUpDTO) => API.post(USER_ROUTES.SIGNUP, data)
+export const me = async () => API.post(USER_ROUTES.ME);
