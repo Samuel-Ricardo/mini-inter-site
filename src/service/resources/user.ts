5f95496713492f1bdab7f9850a270dcabca8a000
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 12:22:07 -0300

    refact

--- src/service/resources/user.ts
+++ src/service/resources/user.ts
@@ -1,8 +1,8 @@
-import { SignInDTO } from "@Types/DTO";
+import { SignInDTO, SignUpDTO } from "@Types/DTO";
 import { API_ROUTES } from "../../config/api";
 import { API } from "../API";
 
 
-const singIn = async (data: SignInDTO) => API.post(API_ROUTES.SIGNIN, data, )
-
-export {};
+export const singIn = async (data: SignInDTO) => API.post(API_ROUTES.SIGNIN, data)
+export const singUp = async (data: SignUpDTO) => API.post(API_ROUTES.SIGNUP, data)
+export const me = async () => API.post(API_ROUTES.ME);
