1c667006f8c5eabd8ccb03d636f27ecbb9ac4cc6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 12:42:27 -0300

    Create: Function - transactions

--- src/service/resources/pix.ts
+++ src/service/resources/pix.ts
@@ -1,7 +1,8 @@
 import { PIX_ROUTES } from '../../config/api'
 import {API} from '../API'
 
-const { PAY, REQUEST, TRANSACTIONS } = PIX_ROUTES;
+const {PAY, REQUEST, TRANSACTIONS} = PIX_ROUTES;
 
 export const request = (value: number) => API.post(REQUEST, { value });
 export const pay = (key: string) => API.post(PAY(key));
+export const transactions = () => API.get(TRANSACTIONS);
