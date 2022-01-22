511506d60ede12f8df724e61bf496bb29763cae8
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 17:27:27 -0300

    Use: Context - auth (./hooks/useAuth.tsx)

---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,8 @@
+import { useContext } from 'react';
+import {AuthContext} from '../context/AuthContext'
+
+const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("No context found");
+  return context
+}
