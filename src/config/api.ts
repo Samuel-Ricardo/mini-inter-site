56f09add55c6e808cf1fd7e622ca42a32e28232b
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 11:10:07 -0300

    Update: File - api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,9 @@
 export const API_ROUTES = {
-  SIGNIN: '/user/signin'
+  SIGNIN: '/user/singin',
+  SIGNUP: '/user/singnup'
 }
 
 export const api = {
   API_ROUTES,
-  BASE_URL: 'https://mini-inter-api.herokuapp.com/'
+  BASE_URL: 'https://mini-inter-api.herokuapp.com'
 }
