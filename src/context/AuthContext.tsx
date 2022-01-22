9ffc4820f498a9e02f8ab11ed5566b41e25723e9
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 13:56:35 -0300

    Create: Function - userSignIn

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,7 @@
-import React, { createContext } from 'react';
+import { SignInDTO } from '@Types/DTO';
+import React, { createContext, useState } from 'react';
+
+import {singIn}  from '../service/resources/user';
 
 interface ContextData {
   id: string,
@@ -12,9 +15,18 @@ interface ContextData {
 
 export const AuthContext = createContext<ContextData>({} as ContextData);
 
-export const AuthProvider:React.FC = ({children}) => {
+export const AuthProvider: React.FC = ({ children }) => {
+
+  const [user, setUser] = useState<ContextData>({} as ContextData);
+
+  const userSignIn = async (userData: SignInDTO) => {
+    
+    const { data } = await singIn(userData);
+    localStorage.setItem('@Inter:Token', data.accessToken);
+  }
+
   return (
-    <AuthContext.Provider value={{}}>
+    <AuthContext.Provider value={user}>
       {children}
     </AuthContext.Provider>
   )
