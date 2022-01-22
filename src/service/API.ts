0f873d81e08d0769f3c7ba27eee9d5dc1c5a87d6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 19:38:34 -0300

    Create: Interceptor - API.ts

--- src/service/API.ts
+++ src/service/API.ts
@@ -1,4 +1,19 @@
 import axios from 'axios';
+import { config } from 'process';
 import { api } from '../config/api';
+import { STORAGE } from '../config/local_storage';
 
-export const API = axios.create({baseURL: api.BASE_URL});
+const {USER} = STORAGE;
+
+const API = axios.create({baseURL: api.BASE_URL});
+
+API.interceptors.request.use(config => {
+
+    const token = localStorage.getItem(USER.TOKEN);
+
+    config.headers = api.AUTHORIZATION(token);
+    return config;
+  }
+)
+
+export {API}
