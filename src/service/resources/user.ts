83e0312be47268f1f58dc1644a64d2f3a3b05cfe
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 19:07:46 -0300

    clean

--- src/service/resources/user.ts
+++ src/service/resources/user.ts
@@ -1,8 +1,10 @@
-import { SignInDTO, SignUpDTO } from "@Types/DTO";
-import { USER_ROUTES } from "../../config/api";
 import { API } from "../API";
+import { USER_ROUTES } from "../../config/api";
+
+import { UserDTO } from "@Type/DTO";
+import { SignInDTO, SignUpDTO } from "@Types/DTO";
 
 
 export const singIn = async (data: SignInDTO) => API.post(USER_ROUTES.SIGNIN, data)
 export const singUp = async (data: SignUpDTO) => API.post(USER_ROUTES.SIGNUP, data)
-export const me = async () => API.get(USER_ROUTES.ME);
+export const me = async () => API.get<UserDTO>(USER_ROUTES.ME);
