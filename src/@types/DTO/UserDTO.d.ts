7756155a061bb413505c861c1a2c5fe5eb9b568c
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 19:06:10 -0300

    Create: @type - UserDTO

--- src/@types/DTO/UserDTO.d.ts
+++ src/@types/DTO/UserDTO.d.ts
@@ -1,3 +1,11 @@
 declare module '@Type/DTO' {
-  
+  export interface UserDTO {
+    id: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    account_number: number,
+    account_digit: number,
+    wallet: number,
+  }
 }
