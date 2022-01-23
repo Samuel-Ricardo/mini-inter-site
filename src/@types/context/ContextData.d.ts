9ed5fd8fc0353b2c0bd03ffda2b2717af23c4e5c
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 22:03:54 -0300

    bugfix

--- src/@types/context/ContextData.d.ts
+++ src/@types/context/ContextData.d.ts
@@ -9,3 +9,9 @@ declare module '@Types/context' {
   }
 }
 
+export interface ContextData {
+  user: UserDTO,
+  userSignIn: (userData: SignInDTO) => Promise<UserDTO>,
+  userSignUp: (userData: SignUpDTO) => Promise<UserDTO>,
+  getCurrentUser: () => Promise<UserDTO>
+}
