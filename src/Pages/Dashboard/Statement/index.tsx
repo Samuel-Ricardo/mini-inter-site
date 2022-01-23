82c224f2b2d4296115e8818570863d4a11a49397
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 21:39:18 -0300

    refact

--- src/Pages/Dashboard/Statement/index.tsx
+++ src/Pages/Dashboard/Statement/index.tsx
@@ -1,54 +1,18 @@
 import { statement } from "@Types/models";
+import { useEffect, useState } from "react";
+import { transactions } from "../../../service/resources/pix";
 import StatementItem from "./Item";
 import { StatementContainer } from "./style";
 
 const Statement = ()  => {
 
-  const statements:statement[] = [
+  const [statements, setStatements] = useState<statement[]>([]);
 
-    {
-      user:{
-          first_name: 'Pablo',
-          last_name: 'Henrique'
-      },
-      value: 250.00,
-      type: 'pay',
-      updatedAt: new Date()
-  },
-  {
-      user:{
-          first_name: 'José',
-          last_name: 'Santos'
-      },
-      value: 270.00,
-      type: 'received',
-      updatedAt: new Date()
-    },
 
-    {
-      user:{
-          first_name: 'Pablo',
-          last_name: 'Henrique'
-      },
-      value: 250.00,
-      type: 'pay',
-      updatedAt: new Date()
-  },
-  {
-      user:{
-          first_name: 'José',
-          last_name: 'Santos'
-      },
-      value: 270.00,
-      type: 'received',
-      updatedAt: new Date()
-  }
-
-  ]
 
   return (
     <StatementContainer>
-      {statements.map(statement => <StatementItem {...statement}/>)}
+      {statements && statements.map(statement => <StatementItem {...statement}/>)}
     </StatementContainer>
   )
  }
