bd6ebbdb6ae810b9004f14aed178bb26b10840c6
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 21:49:46 -0300

    Create: @Type - ContextData

--- src/@types/context/ContextData.d.ts
+++ src/@types/context/ContextData.d.ts
@@ -1,3 +1,10 @@
+import { UserDTO } from "@Type/DTO";
+
 declare module '@Type/context' {
-  
+  interface ContextData {
+    user: UserDTO,
+    userSignIn: (userData: SignInDTO) => Promise<UserDTO>;
+    userSignUp: (userData: SignUpDTO) => Promise<UserDTO>;
+    getCurrentUser: () => Promise<UserDTO>;
+  }
 }
