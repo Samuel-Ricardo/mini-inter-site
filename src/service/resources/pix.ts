9882b813ad418a6bb53e726e7e01ef45c698b075
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-22 21:40:03 -0300

    bugfix

--- src/service/resources/pix.ts
+++ src/service/resources/pix.ts
@@ -1,3 +1,4 @@
+import { statement } from '@Types/models';
 import { PIX_ROUTES } from '../../config/api'
 import {API} from '../API'
 
@@ -5,4 +6,4 @@ const {PAY, REQUEST, TRANSACTIONS} = PIX_ROUTES;
 
 export const request = (value: number) => API.post(REQUEST, { value });
 export const pay = (key: string) => API.post(PAY(key));
-export const transactions = () => API.get(TRANSACTIONS);
+export const transactions = () => API.get<statement[]>(TRANSACTIONS);
