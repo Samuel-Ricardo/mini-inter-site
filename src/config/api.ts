2a70cdc6913b1e07554885b72bf845f3d5f76343
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-25 16:28:27 -0300

    add: development url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,8 +1,8 @@
 import { AxiosRequestHeaders } from 'axios';
 
 export const USER_ROUTES = {
-  SIGNIN: '/user/singin',
-  SIGNUP: '/user/singnup',
+  SIGNIN: '/user/signin',
+  SIGNUP: '/user/signup',
   ME: '/user/me'
 }
 
@@ -20,5 +20,6 @@ export const API_ROUTES = {
 export const api = {
   API_ROUTES,
   BASE_URL: 'https://mini-inter-api.herokuapp.com',
+  BASE_URL_DEVELOPMENT: 'http://localhost:3333',
   AUTHORIZATION: (token: string) => ({'Authorization': `Bearer ${token}`})
 }
