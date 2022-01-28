7acf1eca19e19809d6840bea5c61913c666d438b
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2022-01-28 15:24:53 -0300

    update: style

--- src/Pages/Dashboard/Statement/style.ts
+++ src/Pages/Dashboard/Statement/style.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StatementContainer = styled.div`
   width:100%;
-  margin-top: 20%;
+  margin-top: 10%;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
